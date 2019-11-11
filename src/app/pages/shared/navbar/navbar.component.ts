171a9a4b39572ad0ca9b370b40bf08f696cfe550
Alonso R <luis.alonso.16@hotmail.com>
2019-11-11 02:41:38 -0700

    Fix home active link

--- src/app/pages/shared/navbar/navbar.component.ts
+++ src/app/pages/shared/navbar/navbar.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { MatBottomSheet } from '@angular/material';
 import { ProfileSheetComponent } from '../profile-sheet/profile-sheet.component';
+import { Router, Event, NavigationStart } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  private subject$ = new Subject<void>();
+  isHome = false;
 
-  constructor(private profileSheet: MatBottomSheet) { }
+  constructor(private profileSheet: MatBottomSheet, private router: Router) {
+    this.isHome = (this.router.url === '/') ? true : false;
+  }
 
   ngOnInit() {
+
+    this.router.events.pipe(takeUntil(this.subject$), filter(event => event instanceof NavigationStart))
+        .subscribe((e: NavigationStart) => {
+          this.isHome = (e.url === '/') ? true : false;
+        });
+  }
+
+  ngOnDestroy(): void {
+    this.subject$.next();
+    this.subject$.complete();
   }
 
   onProfile() {
